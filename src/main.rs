fec3d8778a8ee74a5f1bdc7916bd970c60309b56
Illian Yamandú <yamandu.developer@gmail.com>
2024-01-27 22:02:18 -0300

    starting reading file and try except

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::fs;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+    let result = fs::read_to_string("test.txt");
+    let content = match result {
+        Ok(content) => content,
+        Err(error) => return Err(error.into()),
+    };
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
 }
